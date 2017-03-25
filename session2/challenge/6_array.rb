75512e45d284f3444912a5d4934f89158f186775
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-25 16:20:53 +0000

    session 2 - challenge 6

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -1,16 +1,31 @@
 # Write a method named prime_chars? which takes array of strings
-# and returns true if the sum of the characters is prime. 
-# 
-# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself. 
-# 
+# and returns true if the sum of the characters is prime.
+#
+# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
+#
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
 # prime_chars? ['ab', 'c']        # => true
 # prime_chars? ['a', 'b', 'c']    # => true
-# 
+#
 # Examples of length four
 # prime_chars? ['abcd']           # => false
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+
+#require 'prime'
+
+#def prime_chars?(str)
+  #str.join.split("").count.prime?
+#end
+
+
+def prime_chars?(arr)
+  num = arr.join.chars.count
+  return false if num <= 1
+  (2..Math.sqrt(num)).each { |i| return false if num % i == 0   }
+  true
+end
