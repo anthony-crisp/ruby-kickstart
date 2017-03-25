15fd18bb5a2ecd92bb82de9f63f634939bef0f0b
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-25 17:35:46 +0000

    session 2 - challenge 9

--- session2/challenge/9_input_output_logic_string.rb
+++ session2/challenge/9_input_output_logic_string.rb
@@ -24,7 +24,14 @@
 # USER:     BYE
 
 def deaf_grandma
-
+  while input = gets.chomp do
+    break if input == "BYE"
+      if input == input.upcase and input != ""
+        puts "NO, NOT SINCE 1938!"
+      else
+        puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
 end
 
 
