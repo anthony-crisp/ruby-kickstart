684b286a4279d1c508b76df65d695d25c635481a
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-25 15:34:06 +0000

    session 1 - challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  arr = string.split("")
+  arr_two = []
+  arr.each_with_index do |char,index|
+    next_char = arr[index + 1]
+    arr_two << next_char if char == "r" || char == "R"
+ end
+ arr_two.join
 end
