dcb07bda8a7b10fe335557e241e6026350d952bb
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-26 17:15:19 +0100

    session 3 - challenge 2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,13 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(n)
+  new_hash = Hash.new
+  (1..n).each do |k|
+    next if k.even?
+    new_arr  = Array.new(k) { |i| i + 1 }
+    arr_no_odds = new_arr.reject { |i| i.odd? }
+    new_hash[k] = arr_no_odds
+  end
+  new_hash
+end
