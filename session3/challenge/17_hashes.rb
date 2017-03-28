cc4fb8499c6cece8970df78abddce9366f9443b9
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-28 17:30:12 +0100

    session 3 - challenge 17

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,11 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+def print_list_in_reverse(list)
+  return unless list  # The last element in a linked list is always nil, so false, so return the list if is its nil,
+                      # or return the last element.
+  print_list_in_reverse(list[:next])  # Recursive step, passes the next element to print_list_in_reverse,
+                                      #if it's nil, the last element is returned, if not it is passed the next element.
+  puts list[:data]  # puts returns each element of the recursive step to stdout each on a new line,
+                    # so printing the list in reverse order
+end
