7ffe3c6cd87e2e4696037a288e40eee1d668b3a1
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-28 17:29:05 +0100

    session 3 - challenge 16

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,9 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list(list)
+  while list # The last element in a linked list is always nill, so false, so while list is a boolean.
+    puts list[:data] # puts outputs the data to stdout (standard output) on a new line each time.
+    list = list[:next] # Moves to the next element in the linked list, until the last element which is nil so breaks the operation.
+  end
+end
